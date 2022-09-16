9f7b53a0e4bd0963b759f3f426aa0d0af97c170f
juanjo00sg <jujo00sg@gmail.com>
2022-09-16 01:05:46 -0500

    Creación de los métodos para CRUD basados en el proyecto guia

--- app/Controllers/Http/PermisosController.ts
+++ app/Controllers/Http/PermisosController.ts
@@ -1,3 +1,38 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Permiso from 'App/Models/Permiso';
+import EmailService from '../../Services/EmailService';
 
-export default class PermisosController {}
+export default class PermisosController {
+
+    public async index(ctx: HttpContextContract) {
+        /* let permisos: Permiso[] = await Permiso.query().preload('roles')
+        return permisos; */
+        let theEmailService:EmailService= new EmailService();
+        let correo = "juanj.sanchezg@autonoma.edu.co";
+        theEmailService.sendEmail(correo,"Nuevo inicio de sesion", "Usted acaba de iniciar sesion en el sistema");
+    }
+
+    public async store({ request }: HttpContextContract) {
+        const body = request.body();
+        const el_permiso: Permiso = await Permiso.create(body);
+        return el_permiso;
+    }
+
+    public async show({ params }: HttpContextContract) {
+        return Permiso.findOrFail(params.id);
+    }
+
+    public async update({ params, request }: HttpContextContract) {
+        const body = request.body();
+        const el_permiso: Permiso = await Permiso.findOrFail(params.id);
+        el_permiso.url = body.url;
+        el_permiso.metodo = body.metodo;
+        return el_permiso.save();
+    }
+
+    public async destroy({ params }: HttpContextContract) {
+        const el_permiso: Permiso = await Permiso.findOrFail(params.id);
+        return el_permiso.delete();
+    }
+
+}
--- app/Controllers/Http/RolesController.ts
+++ app/Controllers/Http/RolesController.ts
@@ -1,3 +1,43 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Rol from '../../Models/Rol';
+import Usuario from '../../Models/Usuario';
 
-export default class RolsController {}
+export default class RolesController {
+
+    public async index(ctx: HttpContextContract) {
+        let roles: Rol[] = await Rol.query().preload('permisos')
+        return roles;
+    }
+
+    public async store({ request }: HttpContextContract) {
+        const body = request.body();
+        const el_rol: Rol = await Rol.create(body);
+        return el_rol;
+    }
+
+    public async show({ params }: HttpContextContract) {
+        return Rol.findOrFail(params.id);
+    }
+
+    public async update({ params, request }: HttpContextContract) {
+        const body = request.body();
+        const el_rol: Rol = await Rol.findOrFail(params.id);
+        el_rol.nombre = body.nombre;
+        return el_rol.save();
+    }
+    
+    public async destroy({ params }: HttpContextContract) {
+        let usuarios = await Usuario.query()
+            .where(' id_rol ', params.id)
+        if (usuarios) {
+            return {
+                "error": "El rol tiene usuarios asociados ",
+                "usuarios": usuarios
+            }
+        } else {
+            const el_rol: Rol = await Rol.findOrFail(params.id);
+            return el_rol.delete();
+        }
+    }
+
+}
