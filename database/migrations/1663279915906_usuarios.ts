7f10cb30e3364801db8610e3e5fc7f65e4a797e1
juanjo00sg <jujo00sg@gmail.com>
2022-09-16 00:32:27 -0500

    Corrección de migraciones acorde a los diagramas

--- database/migrations/1663279915906_usuarios.ts
+++ database/migrations/1663279915906_usuarios.ts
@@ -3,12 +3,13 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class extends BaseSchema {
   protected tableName = 'usuarios'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table.string('name', 60).notNullable()
       table.string('email', 254).notNullable()
       table.string('password', 256).notNullable().unique()
+      table.integer('id_rol').unsigned().references('roles.id')
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
@@ -18,7 +19,7 @@ export default class extends BaseSchema {
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
--- database/migrations/1663280038238_vuelos.ts
+++ database/migrations/1663280038238_vuelos.ts
@@ -6,8 +6,8 @@ export default class extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.time('hora_salida')
-      table.time('hora_llegada').nullable()
+      table.dateTime('hora_salida')
+      table.dateTime('hora_llegada').nullable()
       table.integer('capacidad')
       table.integer('id_aerolinea').unsigned().references('aerolineas.id')
       table.integer('id_ruta').unsigned().references('rutas.id')
